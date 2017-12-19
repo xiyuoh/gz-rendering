ff85729382f2e6f0be04c032c76153bde11c1489
Shane Loretz <sloretz@osrfoundation.org>
2017-12-19 09:19:27 -0800

    Removed unused code

--- examples/custom_shaders/fragment_shader.glsl
+++ examples/custom_shaders/fragment_shader.glsl
@@ -1,16 +1,4 @@
-//uniform float retro;
-//uniform float near;
-//uniform float far;
-//
-//varying vec4 point;
-
 void main()
 {
-  //vec3 p = vec3(point.x, point.y, point.z - near);
-  //float l = length(point.xyz);
-
-  //if (l>far)
-  //  l = far;
-
   gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);
 }

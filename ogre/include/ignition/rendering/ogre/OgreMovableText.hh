037074f8999b2b58c29ec44cdf803fba9e46bb3b
John Shepherd <john@openrobotics.org>
2019-09-19 15:00:31 -0700

    Update copyright year

--- ogre/include/ignition/rendering/ogre/OgreMovableText.hh
+++ ogre/include/ignition/rendering/ogre/OgreMovableText.hh
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 Open Source Robotics Foundation
+ * Copyright (C) 2019 Open Source Robotics Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,11 +25,6 @@
 
 #include "ignition/rendering/ogre/Export.hh"
 #include "ignition/rendering/ogre/OgreIncludes.hh"
-
-// TODO: remove this line
-//#include "gazebo/rendering/ogre_gazebo.h"
-
-//#include "gazebo/util/system.hh"
 
 namespace ignition
 {
--- ogre/src/OgreMovableText.cc
+++ ogre/src/OgreMovableText.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 Open Source Robotics Foundation
+ * Copyright (C) 2019 Open Source Robotics Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,7 +15,7 @@
  *
 */
 /**
- * File: MovableText.cpp
+ * File: OgreMovableText.cpp
  *
  * description: This create create a billboarding object that display a text.
  *
@@ -30,10 +30,6 @@
 #endif
 
 #include <mutex>
-
-// #include "gazebo/common/common.hh"
-// #include "gazebo/common/Assert.hh"
-// #include "gazebo/rendering/MovableText.hh"
 
 #include "ignition/common/Console.hh"
 #include "ignition/rendering/ogre/OgreMovableText.hh"

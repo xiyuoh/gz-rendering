5fd1ba137c66ffcbbffdea9a3313eff5ef215521
Louise Poubel <louise@openrobotics.org>
2021-11-30 15:54:53 -0800

    Fix global color
    Signed-off-by: Louise Poubel <louise@openrobotics.org>

--- ogre2/src/media/materials/programs/point_fs.glsl
+++ ogre2/src/media/materials/programs/point_fs.glsl
@@ -24,7 +24,6 @@ out vec4 fragColor;
 
 void main()
 {
-  // TODO(louise) Use `color` if per-point coloring isn't used.
   // TODO(louise) It looks like the last point is always blue (and duplicated?)
   fragColor = vec4(ptColor.x, ptColor.y, ptColor.z, 1);
 }
